c0fe0d4cbc8564ee377f414989b7bd42eeb0e7a8
hossam osama <ho4398977@gmail.com>
2025-04-18 09:26:29 +0200

    first commit

---
+++ first_app/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse 
+from django.shortcuts import render
+
+
+
+def index(req):
+
+    return render(req,"index.html",{})
+
+
+
+
+def os(req,number):
+
+    file_name="os"+number
+
+    file_content=""
+
+    with open(f"static/os/{file_name}.txt","r") as f:
+
+        file_content=f.read()
+    
+
+
+    return render(req,"os.html",{"content":file_content})
