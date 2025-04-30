b249a9650234d48ebe63ca105b6813c4f8d1a2f1
hossam osama <ho4398977@gmail.com>
2025-04-30 17:32:52 +0300

    new docker article

--- first_app/urls.py
+++ first_app/urls.py
@@ -7,5 +7,6 @@ from . import views
 
 urlpatterns = [
  path("",views.index,name="index"),
- path("os/<str:number>",views.os,name="os")
+ path("os/<str:number>",views.os,name="os"),
+ path("random/<str:number>",views.random,name="random")
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -30,3 +30,25 @@ def os(req,number):
 
     else:
         return HttpResponse("this file not exist")
+    
+
+def random(req,number):
+
+    file_name="random"+number
+
+    file_content=""
+#/home/hossam123/my_blog/static/os/
+
+    if os_.path.exists(f"/home/hossam123/my_blog/static/random/{file_name}.txt"):
+
+        with open(f"/home/hossam123/my_blog/static/random/{file_name}.txt","r") as f:
+
+            file_content=f.read()
+    
+
+   
+
+        return render(req,"random.html",{"content":file_content})
+
+    else:
+        return HttpResponse("this file not exist")
